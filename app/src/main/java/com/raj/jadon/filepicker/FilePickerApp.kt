7464600022c11d564b8420fccd3a140a4196aff1
raj.jadon <raj.jadon@olaelectric.com>
2022-07-23 04:24:49 +0530

    add hilt

---
+++ app/src/main/java/com/raj/jadon/filepicker/FilePickerApp.kt
@@ -0,0 +1,11 @@
+package com.raj.jadon.filepicker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FilePickerApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
